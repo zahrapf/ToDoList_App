060214839ed0adfefe07c7e3fd56b7523195011c
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-05-22 17:33:54 -0400

    Add layouts

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 let items = ["15 mins Morning Workout"]; 
+let workItems = [];
 
 app.get("/",  (req, res) => {
 
@@ -21,17 +22,35 @@ app.get("/",  (req, res) => {
     let day = today.toLocaleDateString("en-US", option);
 
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newItems: items
     })
 });
 
 app.post("/", (req, res) => {
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else {
+        items.push(item);
+        res.redirect("/");
+    }
 });
 
+app.get("/work", (req,res) => {
+    res.render("list", {
+        listTitle: "Work List",
+        newItems: workItems
+    });
+});
+
+app.post("/work", (req, res)=> {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+});
 
 app.listen(3000, () => {
     console.log("Server is up and running!");
