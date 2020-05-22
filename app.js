f72801d35ae713f64a1bf21dc094b7467e52c025
zahra pourfakhar <zp.fakhar1996@gmail.com>
2020-05-22 14:37:40 -0400

    Display the list of tasks on sc

--- app.js
+++ app.js
@@ -3,41 +3,32 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = []; 
 
 app.get("/",  (req, res) => {
 
     let today = new Date();
-    let day = "";
-    let currentDay = today.getDay();
+    let option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    let day = today.toLocaleDateString("en-US", option);
 
-    switch(currentDay){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error occured when getting the day!");
-    }
+    res.render("list", {
+        kindOfDay: day,
+        newItems: items
+    })
+});
 
-    res.render("list", {kindOfDay: day})
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 
